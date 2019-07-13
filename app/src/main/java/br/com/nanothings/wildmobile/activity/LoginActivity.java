d1129debfc9abfee8dec0804095d0c4d5ce6aa80
Guilherme Ramalho <guilherme-ramalho@outlook.com>
2019-07-13 19:46:29 -0300

    Migrando bibliotecas para AndroidX

--- app/src/main/java/br/com/nanothings/wildmobile/activity/LoginActivity.java
+++ app/src/main/java/br/com/nanothings/wildmobile/activity/LoginActivity.java
@@ -1,6 +1,6 @@
 package br.com.nanothings.wildmobile.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.nanothings.wildmobile.R;
