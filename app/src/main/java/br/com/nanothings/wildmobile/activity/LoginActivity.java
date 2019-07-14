278700edf92d82b53414b3138b87dbdcddc2fa17
Guilherme Ramalho <guilherme-ramalho@outlook.com>
2019-07-13 21:06:06 -0300

    WIP: Implementando LoginActivity

--- app/src/main/java/br/com/nanothings/wildmobile/activity/LoginActivity.java
+++ app/src/main/java/br/com/nanothings/wildmobile/activity/LoginActivity.java
@@ -2,14 +2,28 @@ package br.com.nanothings.wildmobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.nanothings.wildmobile.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.inputUsuario) EditText inputUsuario;
+    @BindView(R.id.inputSenha) EditText inputSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.botaoEntrar)
+    void entrar() {
+        Toast.makeText(this, "Entrando..." + inputUsuario.getText(), Toast.LENGTH_LONG).show();
     }
 }
