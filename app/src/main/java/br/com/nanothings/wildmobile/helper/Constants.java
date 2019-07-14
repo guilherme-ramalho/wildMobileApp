48469c37eae3b4f2527eb4d853e0e0c25b958703
Guilherme Ramalho <guilherme-ramalho@outlook.com>
2019-07-14 17:57:14 -0300

    Alterando URL base

--- app/src/main/java/br/com/nanothings/wildmobile/helper/Constants.java
+++ app/src/main/java/br/com/nanothings/wildmobile/helper/Constants.java
@@ -1,6 +1,5 @@
 package br.com.nanothings.wildmobile.helper;
 
 public class Constants {
-    /*public static final String API_URL = "http://192.168.0.103:8080/";*/
-    public static final String API_URL = "https://jsonplaceholder.typicode.com/";
+    public static final String API_URL = "http://192.168.0.103:8080/";
 }
