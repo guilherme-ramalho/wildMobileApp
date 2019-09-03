7d626ed5250679e7e89dd0865de232ac4f3c3a70
guilherme-ramalho <guilherme-ramalho@outlook.com>
2019-09-02 21:38:32 -0300

    Implementada a activity de apuração

--- app/src/main/java/br/com/nanothings/wildmobile/interfaces/CambistaService.java
+++ app/src/main/java/br/com/nanothings/wildmobile/interfaces/CambistaService.java
@@ -1,15 +1,23 @@
 package br.com.nanothings.wildmobile.interfaces;
 
+import br.com.nanothings.wildmobile.model.Apuracao;
 import br.com.nanothings.wildmobile.model.Cambista;
 import br.com.nanothings.wildmobile.rest.RestObjResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 public interface CambistaService {
     @GET("cambista/autenticar")
     Call<RestObjResponse<Cambista>> autenticar(
             @Header("usuario") String usuario,
             @Header("senha") String senha
+    );
+
+    @GET("cambista/apuracao")
+    Call<RestObjResponse<Apuracao>> apuracao(
+            @Query("dataInicial") String dataInicial,
+            @Query("dataFinal") String dataFinal
     );
 }
